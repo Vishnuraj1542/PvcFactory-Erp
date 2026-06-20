584905f00db5f54accac5ffc2bbf1f677f1ee3f4
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-06-20 23:15:32 +0400

    user registration and rolebased login

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import authenticate,login,logout
 
 # Create your models here.
+class UserAccount(AbstractUser):
+    Usertype_choices=[('Employees','employess'),('Workers','workers')]
+    empid = models.CharField(max_length=12,unique=True)
+    Name = models.CharField(max_length=30,)
+    
