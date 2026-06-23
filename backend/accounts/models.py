5618384e50c665f144381991b664cc1ea3f513bb
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-06-23 22:46:35 +0400

    model designing

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,7 +4,14 @@ from django.contrib.auth import authenticate,login,logout
 
 # Create your models here.
 class UserAccount(AbstractUser):
-    Usertype_choices=[('Employees','employess'),('Workers','workers')]
+    Usertype_choices=[('Admin','admin'),('HR','hr'),('Qc','qc'),('Manager','manager'),('Employees','employees'),]
     empid = models.CharField(max_length=12,unique=True)
-    Name = models.CharField(max_length=30,)
-    
+    created_at=models.DateTimeField(auto_now_add=True,null=True,blank=True)
+
+class PersonalDetails(models.Model):
+    user = models.OneToOneField(UserAccount,null=True,blank=True)
+    image=models.ImageField(upload_to='pictures')
+    passport_number=models.IntegerField(null=True,blank=True)
+    visa_number=models.CharField(max_length=50,null=True,blank=True)
+
+    created_at=models.DateTimeField(auto_now_add=True)
